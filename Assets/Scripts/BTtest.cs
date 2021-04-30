5e9ca782a8407f66b2f672a78eac9a6261d54c3b
JiwoonKim <jiwoon.kim@wrl.onl>
2021-04-30 18:15:05 +0900

    change myDevice.cs

--- Assets/Scripts/BTtest.cs
+++ Assets/Scripts/BTtest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.UI;
 
@@ -14,6 +15,7 @@ public class BTtest : MonoBehaviour
     private int pushed = 0;
     private int connected = 0;
 
+
     private void Start()
     {
         pressedText.text = pushed.ToString();
@@ -24,7 +26,9 @@ public class BTtest : MonoBehaviour
 
         // add our listener to the onDeviceChange event
         InputSystem.onDeviceChange += onInputDeviceChange;
+
         InputSystem.onEvent += onEvent;
+        InputSystem.onEvent += ListenToEvents;
     }
 
     void Update()
@@ -65,23 +69,85 @@ public class BTtest : MonoBehaviour
                 break;
         }
 
+        Debug.Log(device.usages);
+
         connectedText.text = connected.ToString();
     }
 
     public void onEvent(InputEventPtr inputEvent, InputDevice device)
     {
-        //Debug.Log("onEvent");
-
+        //Debug.Log(inputEvent.GetType());
+        //Debug.Log(inputEvent.valid);
         if (inputEvent.sizeInBytes == 28)
         {
-            changeText();
+            changePressedText();
             Debug.Log(device.deviceId);
         }
     }
 
-    public void changeText()
+    public void changePressedText()
     {
         pushed += 1;
-        pressedText.text = pushed.ToString();
+        //GetComponent<writeCSV>().writeTime(t)
+        if(pressedText != null) pressedText.text = pushed.ToString();
     }
+
+    public void changeConnectedText()
+    {
+        if(connectedText != null) connectedText.text = connected.ToString();
+    }
+
+    public void ListenToEvents(InputEventPtr eventPtr, InputDevice device_in)
+    {
+        // Ignore anything that isn't a state event.
+        if (!eventPtr.IsA<StateEvent>() && !eventPtr.IsA<DeltaStateEvent>())
+            return;
+
+        var keyboard = device_in as Keyboard;   //Test for keyboard event
+        var mydevice = device_in as myDevice;
+
+        // Let's say we get some arbitrary InputDevice here as "device".
+        // We allocate some temp memory for a StateEvent and copy
+        // the device's current state into it (both is done by From()).
+
+        using (StateEvent.From(device_in, out var OutEventPtr))
+        {
+            // Let's say we want to toggle on all buttons on the device.
+            foreach (var control in device_in.allControls)
+            {
+                if (control is ButtonControl button)
+                {
+                    //int a = 1;
+                    //button.WriteValueIntoEvent<int>(1, OutEventPtr);
+                }
+            }
+            InputSystem.QueueEvent(OutEventPtr);
+        }
+
+
+        if (mydevice != null)
+        {
+            Debug.Log("mydevice");
+
+            //object state = device_in.ReadValueFromStateAsObject(device_in.GetStatePtrFromStateEvent(eventPtr));
+        }
+        if (keyboard != null)
+        {
+            //Need to take the eventPtr and get the Key press data  **HELP!!!**
+            return;
+        }
+
+        var mouse = device_in as Mouse;   //Test for mouse event
+        if (mouse != null)
+        {
+            //Need to take the eventPtr and get the Mouse button press data **HELP!!!**
+        }
+
+        var gamepad = device_in as Gamepad;      //Test for keyboard event
+        if (gamepad != null)
+        {
+            //Need to take the eventPtr and get the gamepad axis / button press data **HELP!!!**          
+        }
+
+    } // End Listener
 }
---
+++ Assets/Scripts/CsvFileWriter.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class CsvFileWriter : CsvFileCommon, IDisposable
+{
+	// Private members
+	private StreamWriter Writer;
+	private string OneQuote = null;
+	private string TwoQuotes = null;
+	private string QuotedFormat = null;
+
+	/// <summary>
+	/// Initializes a new instance of the CsvFileWriter class for the
+	/// specified stream.
+	/// </summary>
+	/// <param name="stream">The stream to write to</param>
+	///
+	/// 
+	public CsvFileWriter(Stream stream)
+	{
+		Writer = new StreamWriter(stream, Encoding.UTF8);
+	}
+
+	/// <summary>
+	/// Initializes a new instance of the CsvFileWriter class for the
+	/// specified file path.
+	/// </summary>
+	/// <param name="path">The name of the CSV file to write to</param>
+	public CsvFileWriter(string path)
+	{
+		Writer = new StreamWriter(path, true, Encoding.UTF8);
+	}
+
+	/// <summary>
+	/// Writes a row of columns to the current CSV file.
+	/// </summary>
+	/// <param name="columns">The list of columns to write</param>[[p
+	public void WriteRow(List<string> columns)
+	{
+		// Verify required argument-0
+		if (columns == null)
+			throw new ArgumentNullException("columns");
+
+		// Ensure we're using current quote character
+		if (OneQuote == null || OneQuote[0] != Quote)
+		{
+			OneQuote = String.Format("{0}", Quote);
+			TwoQuotes = String.Format("{0}{0}", Quote);
+			QuotedFormat = String.Format("{0}{{0}}{0}", Quote);
+		}
+
+		// Write each column
+		for (int i = 0; i < columns.Count; i++)
+		{
+			if (i > 0)
+				Writer.Write(Delimiter);
+
+			if (columns[i].IndexOfAny(SpecialChars) == -1)
+				Writer.Write(columns[i]);
+			else
+				Writer.Write(QuotedFormat, columns[i].Replace(OneQuote, TwoQuotes));
+		}
+		Writer.WriteLine();
+		Writer.Flush();
+	}
+
+	// Propagate Dispose to StreamWriter
+	public void Dispose()
+	{
+		Writer.Close();
+		Writer.Dispose();
+	}
+}
